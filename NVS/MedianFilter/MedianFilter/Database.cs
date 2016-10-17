d4746757ffd85bd0d80ebab7f71967b42d0a1fe4
Deadmann1 <games.games1@gmx.at>
2016-10-17 17:19:03 +0200

    MedianFilter added Colour Filtering

---
+++ NVS/MedianFilter/MedianFilter/Database.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace MedianFilter
+{
+    class Database
+    {
+        private static Database _instance;
+        public Bitmap ImageBefore;
+        public Bitmap ImageAfter;
+        public List<string> Filters = new List<string>() { "Red", "Blue", "Green" };
+
+        public Database() { }
+
+        public static Database Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Database();
+                }
+                return _instance;
+            }
+        }
+    }
+}
--- NVS/MedianFilter/MedianFilter/MainWindow.xaml.cs
+++ NVS/MedianFilter/MedianFilter/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using AForge.Imaging.Filters;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +28,104 @@ namespace MedianFilter
         public MainWindow()
         {
             InitializeComponent();
+            this.comboBoxFilter.ItemsSource = Database.Instance.Filters;
+            this.comboBoxFilter.SelectedIndex = 0;
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItemLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Please select an image for filtering!";
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+            openFileDialog.DefaultExt = ".png";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Bitmap img = new Bitmap(openFileDialog.FileName);
+                Database.Instance.ImageBefore = img;
+                this.imageBefore.Source = BitmapToImageSource(img);
+            }
+        }
+
+        private void btnFilter_Click(object sender, RoutedEventArgs e)
+        {
+            if(Database.Instance.ImageBefore == null)
+            {
+                MessageBox.Show("You should select an Image before filtering!", "Error:", MessageBoxButton.OK);
+            }
+            else
+            {
+                ChannelFiltering filter = new ChannelFiltering();
+                switch (comboBoxFilter.SelectedItem.ToString())
+                {
+                    case "Red":
+                        filter.Red = new AForge.IntRange(0, 0);
+                        filter.Blue = new AForge.IntRange(0, 255);
+                        filter.Green = new AForge.IntRange(0, 255);
+                        break;
+                    case "Blue":
+                        filter.Red = new AForge.IntRange(0, 255);
+                        filter.Blue = new AForge.IntRange(0, 0);
+                        filter.Green = new AForge.IntRange(0, 255);
+                        break;
+                    case "Green":
+                        filter.Red = new AForge.IntRange(0, 255);
+                        filter.Blue = new AForge.IntRange(0, 255);
+                        filter.Green = new AForge.IntRange(0, 0);
+                        break;
+                }
+                Bitmap tmp = Database.Instance.ImageBefore;
+                Database.Instance.ImageAfter = filter.Apply(tmp);
+                imageAfter.Source = BitmapToImageSource(Database.Instance.ImageAfter);
+            }
+        }
+
+        private BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void MenuItemSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(Database.Instance.ImageAfter == null)
+            {
+                MessageBox.Show("You should select an Image and filter it before trying to save!", "Error:", MessageBoxButton.OK);
+            }
+            else
+            {
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "Images|*.png;*.bmp;*.jpg";
+                ImageFormat format = ImageFormat.Png;
+                if (dialog.ShowDialog() == true)
+                {
+                    string ext = System.IO.Path.GetExtension(dialog.FileName);
+                    switch (ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+                    }
+                    Database.Instance.ImageAfter.Save(dialog.FileName, format);
+                }
+            }
         }
     }
 }
