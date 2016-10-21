2729eacaeec9b73c1661b9d3be4480f23a3ad6d1
Manuel Sammer <games.games1@gmx.at>
2016-10-21 11:59:02 +0200

    Added DBI

---
+++ DBI/Exercises/01_SpatialViewer/01_SpatialViewer/Data/Database.cs
@@ -0,0 +1,65 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SpatialViewer.Data
+{
+    class Database
+    {
+        private static Database instance;
+
+        private Database() { }
+
+        public static Database Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Database();
+                }
+                return instance;
+            }
+        }
+
+        public List<GeoPoint> GetGeoInfo()
+        {
+            string connectionString =
+            "Data Source=D5B12/D5B@//192.168.128.152:1521/ora11g.D5B12";
+            string queryString =
+                "SELECT * FROM straßennetz";
+            using (OracleConnection connection =
+                       new OracleConnection(connectionString))
+            {
+                OracleCommand command = connection.CreateCommand();
+                command.CommandText = queryString;
+
+                try
+                {
+                    connection.Open();
+
+                    OracleDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("ID = {0} Name = {1} Von = {2} Nach = {3} Geotype = {4} Shape = {5}",
+                            reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return null;
+        }
+
+
+
+    }
+}
