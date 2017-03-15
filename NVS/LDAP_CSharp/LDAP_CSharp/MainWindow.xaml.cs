f1ea366aa3aaf681e8308ca5359b133ca92922ec
Manuel Sammer <games.games1@gmx.at>
2017-03-15 07:50:20 +0100

    NVS LDAP

---
+++ NVS/LDAP_CSharp/LDAP_CSharp/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.Protocols;
+using System.Windows.Resources;
+
+namespace LDAP_CSharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "192.168.128.253", "OU=USERS,DC=htl-vil,DC=local"))
+            {
+                // validate the credentials
+                if (pc.ValidateCredentials(txtName.Text, txtPassword.Password))
+                {
+                    label2.Content = "Funktioniert!";
+                    Console.WriteLine("Funktioniert!");
+                    Console.Read();
+                }
+                else
+                {
+                    label2.Content = "Falsch!";
+                    Console.WriteLine("Falsch!");
+                }
+                Console.Read();
+            }
+        }
+    }
+}
--- POS/Exercises/13_BookShop_MVC_V1/web/Login.jsp
+++ POS/Exercises/13_BookShop_MVC_V1/web/Login.jsp
@@ -5,11 +5,13 @@
   Time: 12:48
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c"
+           uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>%Titel$</title>
-
+    <jsp:
 </head>
 <body>
 
